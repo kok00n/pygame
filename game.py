d2a0ede878e989653777a0998575dee2346245c4
kok00n <kamilkonat@gmail.com>
2018-11-14 22:17:51 +0100

    divide program into separate functions add defeat text after car exceeds boundaries

--- game.py
+++ game.py
@@ -1,12 +1,11 @@
 import pygame
+import time
+
 
 pygame.init()
 
 display_width = 800
 display_height = 600
-
-gameDisplay = pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption("A bit Racey")
 
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -14,8 +13,16 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption("A bit Racey")
+clock = pygame.time.Clock()
+
+car_speed = 0
+car_width = 73
+
 # We are loading racecar image to a variable
 carImg = pygame.image.load('racecar.png')
+
 
 # car function places the car on the display
 def car(x, y):
@@ -23,42 +30,67 @@ def car(x, y):
     gameDisplay.blit(carImg, (x, y))
 
 
-# definition of starting points for the car
-x = (display_width * 0.45)
-y = (display_height * 0.8)
-x_change = 0
-car_speed = 0
-car_width = 73
+def crash():
+    message_display('You Crashed')
+    game_loop()
 
-gameExit = False
-clock = pygame.time.Clock()
 
-while not gameExit:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameExit = True
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -5
-            elif event.key == pygame.K_RIGHT:
-                x_change = 5
-
-        if event.type == pygame.KEYUP:
-            if event.key in (pygame.K_LEFT, pygame.K_RIGHT):
-                x_change = 0
-
-    x += x_change
-
-    gameDisplay.fill(white)
-    car(x, y)
-
-    # if car cross the boundaries exit the game
-    if x > display_width - car_width or x < 0:
-        gameExit = True
+def message_display(text):
+    # in Font we can specify font type and font size
+    largeText = pygame.font.Font('freesansbold.ttf', 115)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width / 2), (display_height / 2))
+    gameDisplay.blit(TextSurf, TextRect)
 
     pygame.display.update()
-    clock.tick(60)
+    time.sleep(2)
 
+
+def text_objects(text, font):
+    # True parameter stands for anti-aliasing
+    textSurface = font.render(text, True, black)
+    # get_rect() we get the position of rectangle
+    return textSurface, textSurface.get_rect()
+
+
+def game_loop():
+    # definition of starting points for the car
+    x = (display_width * 0.45)
+    y = (display_height * 0.8)
+
+    x_change = 0
+
+    gameExit = False
+
+    while not gameExit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 5
+
+            if event.type == pygame.KEYUP:
+                if event.key in (pygame.K_LEFT, pygame.K_RIGHT):
+                    x_change = 0
+
+        x += x_change
+
+        gameDisplay.fill(white)
+        car(x, y)
+
+        # if car cross the boundaries exit the game
+        if x > display_width - car_width or x < 0:
+            crash()
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+game_loop()
 pygame.quit()
 quit()
