c71504ed3ecbf0ee8a7e15ab7f9beae9ceb064ab
kok00n <kamilkonat@gmail.com>
2018-11-19 01:45:35 +0100

    big pink circle in the middle of the screen

--- framerate.py
+++ framerate.py
@@ -27,7 +27,12 @@ class FpsView(object):
 
             milliseconds = self.clock.tick(self.fps)
             self.playtime += milliseconds / 1000.0
-            self.draw_text(f"FPS: {self.clock.get_fps():02.2f} Playtime: {self.playtime:4.2f}")
+            #self.draw_text(f"FPS: {self.clock.get_fps():02.2f} Playtime: {self.playtime:4.2f}")
+
+            # Big pink circle in the middle of the screen
+            radius = int(min((self.width, self.height)))
+            self.draw_circle(where=((self.width - radius) // 2, (self.height - radius) // 2), radius=int(radius / 2) , color=(255, 192, 203))
+
             pygame.display.update()
             self.screen.blit(self.background, (0, 0))
 
@@ -37,6 +42,11 @@ class FpsView(object):
         fw, fh = self.font.size(text)
         surface = self.font.render(text, True, (0, 255, 0))
         self.screen.blit(surface, ((self.width - fw) // 2, (self.height - fh) // 2))
+
+    def draw_circle(self, where, radius=25, color=(0, 0, 255)):
+        surface = pygame.Surface((radius * 2, radius * 2))
+        pygame.draw.circle(surface, color, (radius, radius), radius)
+        self.screen.blit(surface, where)
 
 
 if __name__ == '__main__':
